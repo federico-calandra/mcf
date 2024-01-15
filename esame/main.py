4cf539d6ff52ad395079240afa890a8fe71e8ea2
federico <federico.calandra@ymail.com>
2024-01-15 12:35:25 +0100

    minor changes

--- esame/main.py
+++ esame/main.py
@@ -1,7 +1,29 @@
 import rossi
 import numpy as np
 
-""" Questo programma esegue una simulazione montecarlo della propagazione di uno sciame elettromagnetico in un materiale """
+"""
+Questo programma esegue una simulazione montecarlo della propagazione di uno sciame elettromagnetico in un materiale 
+
+Variabili:
+N : int
+    numero di simulazioni da eseguire
+en_ion: list
+    elenco delle energia cedute al materiale ad ogni step
+n_part : list
+    elenco del numero di particelle dello sciame ad ogni step
+mat : Material
+    materiale in cui lo sciame si propaga
+Q : int
+    carica della particella incidente, può essere 0 (fotone), -1 (elettrone) oppure 1 (positrone)
+E0 : float
+    energia in MeV della particella incidente
+sw : Sciame
+    sciame che si propaga nel materiale
+sw_mask : list
+    lista di bool che identifica quali particelle dello sciame hanno abbastanza energia per interagire con il materiale
+is_deterministic : bool
+    se True la propagazione non segue la legge probabilistica
+"""
 
 ## CONFIGURAZIONE SIMULAZIONE
 s,Q,E0,is_det=rossi.config()
@@ -27,9 +49,9 @@ for i in range(1,N+1):
     else:
         mat=rossi.Material('test')
     
-    res=rossi.evolve(mat,sw,sw_mask,is_det,s)
+    sim=rossi.evolve(mat,sw,sw_mask,is_det,s)
     
-    en_ion.append(res[0])
-    n_part.append(res[1])
+    en_ion.append(sim[0])
+    n_part.append(sim[1])
     
 # breakpoint()
